3b3664bcebb110379817e21c9b78b54a50893f6c
ToxyFlog1627 <toxypochta@gmail.com>
2023-11-02 16:10:41 +0200

    highlight

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -3,36 +3,41 @@ static int fuzzy = 1;
 static char font[] = "monospace:size=10";
 static char *fonts[] = {font, "monospace:size=10"};
 static char *prompt = NULL;
-static char normfgcolor[] = "#bbbbbb";
-static char normbgcolor[] = "#222222";
-static char selfgcolor[]  = "#eeeeee";
-static char selbgcolor[]  = "#005577";
-static char outfgcolor[]  = "#eeeeee";
-static char outbgcolor[]  = "#005577";
+static char normfgcolor[]     = "#bbbbbb";
+static char normbgcolor[]     = "#222222";
+static char selfgcolor[]      = "#eeeeee";
+static char selbgcolor[]      = "#005577";
+static char outfgcolor[]      = "#000000";
+static char outbgcolor[]      = "#00ffff";
+static char highnormfgcolor[] = "#ffc978";
+static char highnormbgcolor[] = "#005577";
+static char highselfgcolor[]  = "#ffc978";
+static char highselbgcolor[]  = "#222222";
 static char *colors[SchemeLast][2] = {
- 	/*     fg         bg       */
-	[SchemeNorm] = { normfgcolor, normbgcolor },
-	[SchemeSel]  = { selfgcolor,  selbgcolor  },
-	[SchemeOut]  = { outfgcolor,  outbgcolor },
+	[SchemeNorm]          = { normfgcolor,     normbgcolor     },
+	[SchemeSel]           = { selfgcolor,      selbgcolor      },
+	[SchemeOut]           = { outfgcolor,      outbgcolor      },
+	[SchemeSelHighlight]  = { highnormfgcolor, highnormbgcolor },
+	[SchemeNormHighlight] = { highselfgcolor,  highselbgcolor  },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
 
-/*
- * Xresources preferences to load at startup
- */
 ResourcePref resources[] = {
-	{ "font",        STRING, &font },
-	{ "normfgcolor", STRING, &normfgcolor },
-	{ "normbgcolor", STRING, &normbgcolor },
-	{ "selfgcolor",  STRING, &selfgcolor },
-	{ "selbgcolor",  STRING, &selbgcolor },
-	{ "outfgcolor",  STRING, &outfgcolor },
-	{ "outbgcolor",  STRING, &outbgcolor }
+	{ "font",            STRING, &font            },
+	{ "normfgcolor",     STRING, &normfgcolor     },
+	{ "normbgcolor",     STRING, &normbgcolor     },
+	{ "selfgcolor",      STRING, &selfgcolor      },
+	{ "selbgcolor",      STRING, &selbgcolor      },
+	{ "outfgcolor",      STRING, &outfgcolor      },
+	{ "outbgcolor",      STRING, &outbgcolor      },
+    { "highnormfgcolor", STRING, &highnormfgcolor },
+    { "highnormbgcolor", STRING, &highnormbgcolor },
+    { "highselfgcolor",  STRING, &highselfgcolor  },
+    { "highselbgcolor",  STRING, &highselbgcolor  }
 };
